c4f3df1ecbe7dfb26a487976bf0e0214b19107b4
Елена Руменова Кесерджиева <ERKeserdzhieva20@codingburgas.bg>
2023-10-10 09:43:22 +0300

    Submit classwork

---
+++ Zadachi/Zadachi/src/threeLineNumbers.java
@@ -0,0 +1,10 @@
+class threeLineNumbers {
+    public static void main(String[] args) {
+        for (int j = 0; j < 3; j++) {
+            for (int i = 1; i <= 5; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
